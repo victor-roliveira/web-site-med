562e4e4cf771f2e432efc2493cacb4f1e739441e
VictoROliveira-Dev <victorramos123@outlook.com>
2024-09-10 16:31:39 -0300

    feat: add SEO in the course hemogasometria

--- src/pages/CursoHemogasometria.tsx
+++ src/pages/CursoHemogasometria.tsx
@@ -2,10 +2,24 @@ import Footer from "@/components/footer";
 import MainSection from "@/components/course-hemo-section/main-section";
 import ResourcesSection from "@/components/course-hemo-section/resources-section";
 import PaymentSection from "@/components/course-hemo-section/payment-section";
+import { Helmet } from "react-helmet-async";
 
 function CursoHemogasometria() {
   return (
     <>
+    <Helmet>
+        <title>Hemogasometria | Descomplicando</title>
+        <meta name="description" content="Bem vindo ao curso de Hemogasometria." />
+        <meta name="keywords" content="courses, education, learning" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="/" />
+
+        <meta property="og:title" content="Hemogasometria | Descomplicando" />
+        <meta property="og:description" content="Bem vindo ao curso de Hemogasometria." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.descomplicando.com.br/" />
+        <meta property="og:image" content="../assets/logo-descomplicando.png" />
+      </Helmet>
       <MainSection />
 
       <ResourcesSection />
